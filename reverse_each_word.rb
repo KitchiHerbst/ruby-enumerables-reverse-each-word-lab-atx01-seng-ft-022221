d88380f5a668ac727e453ed2613e6f6c422ad940
Triston Herbst <tristonherbst97@gmail.com>
2021-02-02 15:45:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,3 +12,19 @@ def reverse_each_word(string)
   
 end
 
+
+
+
+
+
+
+
+def reverse_each_word(string)
+  string_to_array = string.split(' ')
+  new_array = [ ]
+  string_to_array.collect do |element|
+    element.reverse!
+    new_array << element
+  end
+  new_array.join(" ")
+end
